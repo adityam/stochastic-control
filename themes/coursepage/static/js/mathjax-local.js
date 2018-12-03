199ee2957cc9feb8d46122ed0a52f074a1c13892
Aditya Mahajan <adityam@umich.edu>
2018-12-03 01:27:52 -0500

    Use local mathjax config

---
+++ themes/coursepage/static/js/mathjax-local.js
@@ -0,0 +1,29 @@
+MathJax.Hub.Config({
+  "HTML-CSS": { 
+      fonts: ["TeX"]
+  }, 
+tex2jax: {
+  inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+  displayMath: [ ['$$','$$'], ["\\[","\\]"] ],
+  processEscapes: true,
+},
+  TeX : {
+      Macros : {
+        PR: "\\mathbb{P}",
+        EXP: "\\mathbb{E}",
+        IND: "\\mathbb{I}",
+        reals: "\\mathbb{R}",
+        integers: "\\mathbb{Z}",
+        TRANS: "\\intercal",
+        VEC: "\\operatorname{vec}",
+        TR:  "\\operatorname{Tr}",
+        // mathcal: "\\mathscr",
+        ALPHABET: ["\\mathcal{#1}", 1],
+        MATRIX: ["\\begin{bmatrix} #1 \\end{bmatrix}", 1],
+        NORM: ["\\left\\lVert #1 \\right\\rVert", 1],
+        ABS: ["\\left\\lvert #1 \\right\\rvert", 1],
+      }
+  }
+});
+
+Mathjax.Ajax.loadcomplete("https://adityam.github.io/stochastic-control/js/mathjax-local.js")
