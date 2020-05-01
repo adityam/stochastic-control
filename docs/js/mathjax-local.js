942c99eaabba2e119b9ad05d6528b978980da453
Aditya Mahajan <adityam@umich.edu>
2020-05-01 06:47:00 -0400

    Updated

--- docs/js/mathjax-local.js
+++ docs/js/mathjax-local.js
@@ -20,15 +20,10 @@ window.MathJax = {
     },
     autoload: {
       color: [],
-      colorV2: ['color']
     },
-    packages: {'[+]': ['noerrors']}
   },
   options: {
     ignoreHtmlClass: 'tex2jax_ignore',
     processHtmlClass: 'tex2jax_process'
   },
-  loader: {
-    load: ['[tex]/noerrors']
-  }
 };
