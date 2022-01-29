3802ecd64ae4df9cc4ea6738aba2376b59fe305d
Aditya Mahajan <adityam@umich.edu>
2022-01-28 23:27:23 -0500

    Updated code for A1

--- content/solutions/01/static-2.jl
+++ content/solutions/01/static-2.jl
@@ -1,30 +1,30 @@
 using LinearAlgebra: dot, argmin
 
 # The state spaces
-X, Y, U, W = 1:2, 1:2, 1:3, 1:3
+S, Y, A, W = 1:2, 1:2, 1:3, 1:3
 
 # The transition matrix is called Q in the
 # question. We call it P for convenience
 
-P = zeros(length(X), length(W), length(Y));
+P = zeros(length(S), length(W), length(Y));
 
 P[:,:,1] = [0.15 0.1 0.0; 0.15 0.05 0.1];
 P[:,:,2] = [0.1 0.05 0.05; 0.15 0.05 0.05];
 
 # Cost
-C = zeros(length(X), length(U), length(W));
+C = zeros(length(S), length(A), length(W));
 C[:,:,1] = [3 5 1; 2 3 1];
 C[:,:,2] = [4 3 1; 1 2 8];
 C[:,:,3] = [1 2 2; 4 1 3];
 
-Q = zeros(length(X), length(Y), length(U));
-for x in X, y in Y, u in U
-    Q[x,y,u] = dot(P[x,:,y], C[x,u,:])
+Q = zeros(length(S), length(Y), length(A));
+for s in S, y in Y, a in A
+    Q[s,y,a] = dot(P[s,:,y], C[s,a,:])
 end
 
-g = zeros(Int, length(X), length(Y));
-for x in X, y in Y
-    g[x,y] = argmin(Q[x,y,:])
+π = zeros(Int, length(S), length(Y));
+for s in S, y in Y
+    π[s,y] = argmin(Q[s,y,:])
 end
 
-@info "The optimal actions are" g 
+@info "The optimal actions are" π 
--- content/solutions/01/static.jl
+++ content/solutions/01/static.jl
@@ -1,24 +1,24 @@
 using LinearAlgebra: dot, argmin
 
 # The state spaces
-X, U, W = 1:2, 1:3, 1:3
+S, A, W = 1:2, 1:3, 1:3
 
 # Probability transition matrix
 P = [0.25 0.15 0.05; 0.30 0.10 0.15]
 
 # Cost function
-C = zeros(length(X), length(U), length(W))
+C = zeros(length(S), length(A), length(W))
 C[:,:,1] = [3 5 1; 2 3 1];
 C[:,:,2] = [4 3 1; 1 2 8];
 C[:,:,3] = [1 2 2; 4 1 3];
 
-Q = zeros(length(X), length(U));
-for x in X, u in U
-    Q[x,u] = dot(P[x,:],C[x,u,:])
+Q = zeros(length(S), length(A));
+for s in S, a in A
+    Q[s,a] = dot(P[s,:],C[s,a,:])
 end
 
-g = zeros(Int, length(X));
-for x in X
-    @views g[x] = argmin(Q[x,:])
+π = zeros(Int, length(S));
+for s in S
+    @views π[s] = argmin(Q[s,:])
 end
-@info "The optimal actions are" g 
+@info "The optimal actions are" π
